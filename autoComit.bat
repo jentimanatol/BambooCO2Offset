5dd21f78ccce822206de28db756a6baa9d99bf21
Anatolie Jentimir <157539682+jentimanatol@users.noreply.github.com>
2025-05-04 22:35:41 -0400

    ' pip install seaborn'

--- autoComit.bat
+++ autoComit.bat
@@ -1,9 +1,9 @@
 git --version
 git add .
-git commit -m "' Time Series Plot Option'"
+git commit -m "' pip install seaborn'"
 git push origin main
 
 :: === Tagging for GitHub Actions Release Build ===
-git tag v2.2
-git push origin v2.2
+git tag v2.3
+git push origin v2.3
 pause
